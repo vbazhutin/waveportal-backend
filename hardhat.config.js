b4a342222817089c9b574e898aa236479de5372e
vbazhutin <vladimir.bazhutin@gmail.com>
2022-12-04 19:20:48 -0500

    updated hardhat config .env values

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,6 +6,10 @@ require("hardhat-gas-reporter");
 require("hardhat-contract-sizer");
 require("dotenv").config();
 
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+
 module.exports = {
 	defaultNetwork: "hardhat",
 	networks: {
